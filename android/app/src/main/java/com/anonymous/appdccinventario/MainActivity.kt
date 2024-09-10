1921a7fb7c95b460da6065de4797e2305b5409b8
melomatheuss <matheus.syn.eco@gmail.com>
2024-09-09 21:43:07 -0400

    bug google service fixed

--- android/app/src/main/java/com/anonymous/appdccinventario/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/appdccinventario/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.appdccinventario
+package com.anonymous.appdccinventario
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/appdccinventario/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/appdccinventario/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.appdccinventario
+package com.anonymous.appdccinventario
 
 import android.app.Application
 import android.content.res.Configuration
